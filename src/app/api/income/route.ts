7d47967085fb7d2b48030faaa9b02b68880dcf32
Chris Anderson <chris.d.anderson87@gmail.com>
2024-07-26 16:36:30 +1200

    restructuring route into api folder

---
+++ src/app/api/income/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../../lib/db";
+
+export async function GET() {
+  const incomes = await prisma.income.findMany();
+  return NextResponse.json(incomes);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const newIncome = await prisma.income.create({
+    data: body,
+  });
+  return NextResponse.json(newIncome);
+}
