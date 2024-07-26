792531541f4205a5ac82e681d0597853e6797229
Chris Anderson <chris.d.anderson87@gmail.com>
2024-07-26 15:01:12 +1200

    updating reducers

--- src/redux/slices/incomeSlice.ts
+++ src/redux/slices/incomeSlice.ts
@@ -1,32 +1,47 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface IncomeState {
+export interface Income {
+  id: number;
   amount: number;
   date: string;
   type: string;
 }
 
-const initialState: IncomeState[] = [];
+interface IncomeState {
+  incomes: Income[];
+}
+
+const initialState: IncomeState = {
+  incomes: [],
+};
 
 const incomeSlice = createSlice({
   name: "income",
   initialState,
   reducers: {
-    addIncome: (state, action: PayloadAction<IncomeState>) => {
-      state.push(action.payload);
+    addIncome: (state, action: PayloadAction<Income>) => {
+      state.incomes.push(action.payload);
     },
-    updateIncome: (
-      state,
-      action: PayloadAction<{ index: number; income: IncomeState }>
-    ) => {
-      state[action.payload.index] = action.payload.income;
+    setIncomes: (state, action: PayloadAction<Income[]>) => {
+      state.incomes = action.payload;
+    },
+    updateIncome: (state, action: PayloadAction<Income>) => {
+      const index = state.incomes.findIndex(
+        (income) => income.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.incomes[index] = action.payload;
+      }
     },
     deleteIncome: (state, action: PayloadAction<number>) => {
-      state.splice(action.payload, 1);
+      state.incomes = state.incomes.filter(
+        (income) => income.id !== action.payload
+      );
     },
   },
 });
 
-export const { addIncome, updateIncome, deleteIncome } = incomeSlice.actions;
-
+export const { addIncome, setIncomes, updateIncome, deleteIncome } =
+  incomeSlice.actions;
 export default incomeSlice.reducer;
+export type { Income, IncomeState };
