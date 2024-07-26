8f0f8cf37e068049c8130d258e92761ee2cab77a
Chris Anderson <chris.d.anderson87@gmail.com>
2024-07-26 14:08:30 +1200

    setup incomeSlice

---
+++ src/redux/slices/incomeSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IncomeState {
+  income: number;
+  date: string;
+  type: string;
+}
+
+const initialState: IncomeState[] = [];
+
+const incomeSlice = createSlice({
+  name: "income",
+  initialState,
+  reducers: {
+    addIncome: (state, action: PayloadAction<IncomeState>) => {
+      state.push(action.payload);
+    },
+    updateIncome: (
+      state,
+      action: PayloadAction<{ index: number; income: IncomeState }>
+    ) => {
+      state[action.payload.index] = action.payload.income;
+    },
+    deleteIncome: (state, action: PayloadAction<number>) => {
+      state.splice(action.payload, 1);
+    },
+  },
+});
+
+export const { addIncome, updateIncome, deleteIncome } = incomeSlice.actions;
+
+export default incomeSlice.reducer;
