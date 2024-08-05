e2d68d9f973432a7e29f1301cbd17883b93c2f68
Chris Anderson <chris.d.anderson87@gmail.com>
2024-08-05 19:31:03 +1200

    /income rendering locally. issues with docker todo

--- lib/db.ts
+++ lib/db.ts
@@ -4,12 +4,15 @@ const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// Extend global type with prismaGlobal
+declare global {
+  var prismaGlobal: PrismaClient | undefined;
+}
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = global.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
--- src/app/income/page.tsx
+++ src/app/income/page.tsx
@@ -3,8 +3,11 @@ import IncomeForm from "@/components/income/IncomeForm";
 import IncomeList from "@/components/income/IncomeList";
 
 export default async function IncomePage() {
-  const res = await fetch("/income");
+  const res = await fetch(`http://localhost:3000/api/income`, {
+    cache: "no-store", // Avoid caching issues
+  });
   const data = await res.json();
+
   return (
     <ReduxProvider>
       <div>
